8c46f2b5447d9669aaac6da26a42da0b9ca3db5b
Robert Yuen <dungeonry@hotmail.com>
2016-12-19 14:57:12 -0800

    Neglected to add nodemon to dependencies.  Moved some code fron consolebot.js to index.js

--- index.js
+++ index.js
@@ -12,8 +12,25 @@ const connector = new builder.ChatConnector({
 });
 var bot = new builder.UniversalBot(connector);
 
+const dialog = new builder.IntentDialog()
+  .matches(/^load$/i, [
+    (session) => {
+      builder.Prompts.text(session, "Please enter the name");
+    },
+    (session, results) => {
+      session.endConversation(`You're looking for ${results.response}`);
+    }])
+  .onDefault((session) => {
+    session.endConversation("Hi there! I'm a GitHub bot. I can load user profile information if you send the command **load**.");
+  });
+
 server.post("/api/messages", connector.listen());
 
-bot.dialog("/", (session) => {
-  session.send("Hello, Thank you for taking the time.");
-});
+bot.dialog("/", dialog);
+
+bot.beginDialogAction("help", "/help", {matches: /^help/});
+bot.dialog("/help", [
+  (session) => {
+    session.endDialog("This bot allows you to load gh data.");
+  }
+]);
